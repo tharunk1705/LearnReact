46274dd28fea63ba70f28eefc80bddeb6f422df3
Tharun Kumar <tharunk1705@gmail.com>
2020-09-25 00:13:54 +0530

    Single Page Applications Part 2

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes'
+import Contact from './ContactComponent';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -15,7 +19,9 @@ class Main extends React.Component {
 
     this.state = {
       dishes : DISHES,
-      selectedDish : null
+      comments : COMMENTS,
+      promotions : PROMOTIONS,
+      leaders : LEADERS
     };
   }
   
@@ -24,16 +30,28 @@ class Main extends React.Component {
 
     const HomePage = () => {
       return(
-        <Home/>
+        <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+    
     return (
       <div>
         <Header />
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Redirect to="/home" />
           </Switch>
         <Footer />
--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 import Dishdetail from './DishdetailComponent';
 
     function RenderMenuItem({dish, onClick}){
          return(
-            <Card onClick={()=>onClick(dish.id)} >
-                <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                <CardImgOverlay>
-                    <CardTitle >{dish.name}</CardTitle>
-                </CardImgOverlay>
+            <Card >
+                <Link to = {`/menu/${dish.id}`} >
+                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                    <CardImgOverlay>
+                        <CardTitle >{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Link>
             </Card>
          );
     }
@@ -17,13 +22,23 @@ import Dishdetail from './DishdetailComponent';
         const menu = props.dishes.map((dish) =>{
             return(
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <RenderMenuItem dish={dish} onClick={props.onClick}/>
+                    <RenderMenuItem dish={dish}/>
                 </div>
             ) ;
         });
 
         return(
             <div className = "conatiner">
+                <div className = "row">
+                       <Breadcrumb>
+                            <BreadcrumbItem><Link to='/home'>Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Menu</BreadcrumbItem>
+                       </Breadcrumb>
+                       <div className='col-12'>
+                           <h3>Menu</h3>
+                           <hr/>
+                       </div>
+                </div>
                 <div className = "row">
                        {menu}
                 </div>
